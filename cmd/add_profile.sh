356ad3e018c2210621e40dce13ef19992d1b2266
Jakrin Juangbhanich <juangbhanich.k@gmail.com>
2018-04-11 12:37:09 +0800

    Fixed shell script.

--- cmd/add_profile.sh
+++ cmd/add_profile.sh
@@ -3,7 +3,15 @@
 
 add_list_id=b7266f75-b49b-4c90-966c-e39c32f37edb
 old_list=$(dconf read /org/gnome/terminal/legacy/profiles:/list | tr -d "]")
-new_list="$old_list, '$add_list_id']"
+
+if [ -z "$old_list" ]
+then
+	front_list="["
+else
+	front_list="$old_list, "
+fi
+
+new_list="$front_list'$add_list_id']"
 dconf write /org/gnome/terminal/legacy/profiles:/list "$new_list" 
 dconf write /org/gnome/terminal/legacy/profiles:/default "'$add_list_id'"
 
